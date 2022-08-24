f41e8c54c7cafd13a781673797b2a5787e012c11
Jure Bogunovic <bogunovij@gmail.com>
2022-08-24 21:13:13 +0200

    removed app

--- app/src/polywrap/loader/IPackageLoader.ts
+++
@@ -1,5 +0,0 @@
-import { IWrapPackage } from "../wrap/IWrapPackage";
-
-export interface IPackageLoader {
-  load(packagePath: string): Promise<IWrapPackage>
-}
--- app/src/polywrap/wasm/IWasmPackage.ts
+++
@@ -1,5 +0,0 @@
-import { IWrapPackage } from "../wrap/IWrapPackage";
-
-export interface IWasmPackage extends IWrapPackage {
-  getWasmModule(): Promise<Uint8Array | undefined>;
-}
